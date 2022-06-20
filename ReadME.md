4ec6baa0bb0363e8b3bc1658af121c570d4f770f
Eric Sanchez <107879816+ERICS701@users.noreply.github.com>
2022-06-20 19:03:22 +0000

    notes

---
+++ ReadME.md
@@ -0,0 +1,17 @@
+## input 
+input is the way the computer gets informattion from the user 
+
+notes: python inttially treats input as a `string ` type 
+
+ex.
+
+5--> "5"
+
+today is monday --> today is monday 
+
+### formula:
+`variablename =input("prompt")
+
+prompt - a question or statment that the computer asks the user 
+
+python stores the __input__ in the __variablename__
---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+username =input("enter your name ")
+print("hey " + username)
+
+usergrade = input ("what grade are you in?")
+print ("hey" + username + "," + usergrade + "is a lame grade")
+
+useryear = int(input("what year were you born? "))
+userAge = 2022 - useryear
+print (str(userAge) + " is so young ")
+'''
+
+# create your own madlibs
+# have at least 5 input
+noun = input("please give me a noun: ")
+userfavoitegames = input("Enter your favoite game name ") 
+userfavoitefood = input(" Enter your favoite  food")
+userfavoitecandy = input(" Enter your favoite candy ")
+userfavoitemovie = input(" Enter your favoite movie")
+userfavoitesport = input(" Enter your favoite sport  ")
